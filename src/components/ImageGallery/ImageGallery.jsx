d9dfee6ead3b11da0474ed5555bd6421103c80bf
Reshte <107108359+Reshte@users.noreply.github.com>
2023-01-17 21:07:36 +0200

    edits

--- src/components/ImageGallery/ImageGallery.jsx
+++ src/components/ImageGallery/ImageGallery.jsx
@@ -5,6 +5,7 @@ import { Loader } from 'components/Loader/Loader'
 import { ImageGalleryList, Wrapper } from './ImageGallery.styled'
 
 
+
 export class ImageGallery extends Component {
     state = {
         gallery: [],
@@ -14,15 +15,17 @@ export class ImageGallery extends Component {
     async componentDidUpdate(prevProps, prevState) {
       
         if (prevProps.searchQuery !== this.props.searchQuery) {
-              this.setState({ loading: true })
-        const response = await requestApi(this.props.page, this.props.searchQuery)
-               this.setState(prevState => { return { gallery: [...response.hits], loading: false } })
-        }
+            this.setState({ loading: true })
+            const response = await requestApi(this.props.page, this.props.searchQuery)
+            if(response.hits.length >0){this.setState(prevState => { return { gallery: [...response.hits], loading: false } })} 
+            else{ this.setState(prevState => {return { gallery: [], loading: false } })
+                alert('Sorry, there are no images matching your search query. Please try again.')}
+               }
 
         if (prevProps.page !== this.props.page) {
               this.setState({ loading: true })
-        const response = await requestApi(this.props.page, this.props.searchQuery)
-            this.setState(prevState => { return { gallery: [...prevState.gallery, ...response.hits], loading: false } })
+             const response = await requestApi(this.props.page, this.props.searchQuery)
+             this.setState(prevState => { return { gallery: [...prevState.gallery, ...response.hits], loading: false } })
         }
        
     }
